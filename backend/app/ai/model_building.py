c2370eee61837a77bd40d2d4ab2c0b34874572ef
ktxdev <sean.ktxdev@gmail.com>
2024-07-17 05:07:31 -0400

    Added EDA

--- backend/app/ai/model_building.py
+++ backend/app/ai/model_building.py
@@ -1,36 +1,37 @@
-import string
 import nltk
+import string
 import joblib
 import pandas as pd
 
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
-from sklearn.preprocessing import LabelEncoder
 from datasets import load_dataset
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import GridSearchCV, StratifiedKFold, train_test_split
+from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
+
 
 models = {
-    'Logistic Regression': (LogisticRegression(), {
-        'C': [0.1, 1, 10],
-        'solver': ['liblinear']
-    }),
-    'Support Vector Machine': (SVC(), {
-        'C': [0.1, 1, 10],
-        'kernel': ['linear', 'rbf']
-    }),
-    'Random Forest': (RandomForestClassifier(), {
-        'n_estimators': [10, 100, 200],
-        'max_depth': [None, 10, 20]
-    }),
-    'k-Nearest Neighbors': (KNeighborsClassifier(), {
-        'n_neighbors': [3, 5, 11],
-        'weights': ['uniform', 'distance']
-    }),
+    # 'Logistic Regression': (LogisticRegression(multi_class='auto'), {
+    #     'C': [0.1, 1, 10],
+    #     'solver': ['liblinear']
+    # }),
+    # 'Support Vector Machine': (SVC(), {
+    #     'C': [0.1, 1, 10],
+    #     'kernel': ['linear', 'rbf']
+    # }),
+    # 'Random Forest': (RandomForestClassifier(), {
+    #     'n_estimators': [10, 100, 200],
+    #     'max_depth': [None, 10, 20]
+    # }),
+    # 'k-Nearest Neighbors': (KNeighborsClassifier(), {
+    #     'n_neighbors': [3, 5, 11],
+    #     'weights': ['uniform', 'distance']
+    # }),
     'Decision Tree': (DecisionTreeClassifier(), {
         'max_depth': [None, 10, 20],
         'min_samples_split': [2, 5, 10]
@@ -39,7 +40,7 @@ models = {
 
 # Download stopwords from nltk
 nltk.download('stopwords')
-stop_words = set(stopwords.words('english'))
+stop_words = set(nltk.corpus.stopwords.words('english'))
 
 # Function to preprocess text
 def preprocess_text(text):
@@ -52,26 +53,28 @@ def preprocess_text(text):
     return text
 
 def train_models(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    sss = StratifiedKFold(n_splits=2)
 
     # Test each model
     best_models = {}
     for model_name, (model, params) in models.items():
-        grid_search = GridSearchCV(model, params, cv=5, scoring='accuracy', n_jobs=-1)
-        grid_search.fit(X_train, y_train)
+        grid_search = GridSearchCV(model, params, cv=sss, scoring='accuracy', n_jobs=-1)
+        grid_search.fit(X, y)
 
-        best_model = grid_search.best_estimator_
-        y_pred = best_model.predict(X_test)
+        # best_model = grid_search.best_estimator_
+        # y_pred = best_model.predict(X_test)
 
-        accuracy = accuracy_score(y_test, y_pred)
-        roc_auc = roc_auc_score(y_test, best_model.predict_proba(X_test)[:, 1]) if hasattr(best_model, "predict_proba") else None
+        # accuracy = accuracy_score(y_test, y_pred)
+        # roc_auc = roc_auc_score(y_test, best_model.predict_proba(X_test)[:, 1]) if hasattr(best_model, "predict_proba") else None
 
         best_models[model_name] = {
-            'best_model': best_model,
+            'best_model': grid_search.best_estimator_,
             'best_params': grid_search.best_params_,
-            'accuracy': accuracy,
-            'roc_auc': roc_auc,
-            'classification_report': classification_report(y_test, y_pred)
+            'best_score': grid_search.best_score_,
+            # 'accuracy': accuracy,
+            # 'roc_auc': roc_auc,
+            # 'classification_report': classification_report(y_test, y_pred)
         }
     
     return best_models
@@ -102,25 +105,14 @@ if __name__ == "__main__":
     for model_name, result in best_models.items():
         print(f"Model: {model_name}")
         print(f"Best Parameters: {result['best_params']}")
-        print(f"Accuracy: {result['accuracy']}")
-        if result['roc_auc'] is not None:
-            print(f"ROC-AUC: {result['roc_auc']}")
-        print(f"Classification Report:\n{result['classification_report']}")
-        print("\n" + "="*80 + "\n")
+        print(f"Best Score: {result['best_score']}")
+        # if result['roc_auc'] is not None:
+        #     print(f"ROC-AUC: {result['roc_auc']}")
+        # print(f"Classification Report:\n{result['classification_report']}")
+        # print("\n" + "="*80 + "\n")
 
         # Save the model, vectorizer, and label encoder
-        joblib.dump(result['best_model'], f'{model_name.replace(" ", "_")}_model.pkl')
+        joblib.dump(result['best_model'], f'data/models/{model_name.replace(" ", "_")}_model.pkl')
 
-    joblib.dump(vectorizer, 'tfidf_vectorizer.pkl')
-    joblib.dump(label_encoder, 'label_encoder.pkl')
-
-
-    
-
-
-
-
-
-
-
-
+    joblib.dump(vectorizer, 'data/models/tfidf_vectorizer.pkl')
+    joblib.dump(label_encoder, 'data/models/label_encoder.pkl')
---
+++ backend/app/ai/predict.py
@@ -0,0 +1,3 @@
+
+def predict_disease():
+    pass
